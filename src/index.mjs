67414c2bd1febc1332c07aa10dccaafddb59a8ea
jascha <jascha@jaeh.at>
2020-04-28 20:39:50 +0200

    use createServer from @grundstein/commons

--- src/index.mjs
+++ src/index.mjs
@@ -1,27 +1,17 @@
-import http from 'http'
-
 import { log } from '@grundstein/commons'
-import * as middleware from '@grundstein/commons/middleware.mjs'
-// import { getHostCertificates } from '@grundstein/commons/lib.mjs'
+
+import { createServer } from '@grundstein/commons/lib.mjs'
 
 import initApi from './api.mjs'
 import handler from './handler.mjs'
 
 export const run = async (config = {}) => {
-  config.startTime = log.hrtime()
-
   try {
+    config.startTime = log.hrtime()
+
     const api = await initApi(config)
 
-    const options = {} // await getHostCertificates(config)
-
-    const server = http.createServer(options, handler(api))
-
-    const clientError = middleware.clientError(config)
-    server.on('clientError', clientError)
-
-    const listener = middleware.listener(config)
-    server.listen(config.port, config.host, listener)
+    await createServer(config, handler(api))
   } catch (e) {
     log.error(e)
     process.exit(1)
