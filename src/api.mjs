aa560e4e34181542049088242bd0be6c9e0dbfff
jaeh <jascha@jaeh.at>
2023-09-30 19:35:58 +0200

    src/api: load __getData__ file from each version of the api

--- src/api.mjs
+++ src/api.mjs
@@ -15,33 +15,50 @@ export const initApi = async config => {
 
   const startTime = log.hrtime()
 
-  const api = {}
+  const api = {
+    hosts: {},
+  }
 
   log.info(`@grundstein/gas: serving api from ${dir}`)
-
-  let { dataFile } = config
-  if (!path.isAbsolute(dataFile)) {
-    dataFile = path.join(dir, dataFile)
-  }
-
-  const dataFileExists = await fs.exists(dataFile)
-  if (dataFileExists) {
-    const urlPath = pathToFileURL(dataFile)
-    const { getData } = await import(urlPath)
-    const { db, schema } = await getData()
-    api.db = db
-    api.schema = schema
-  }
 
   const files = await fs.getFiles(dir)
 
   await Promise.all(
     files.map(async file => {
       const relativePath = file.replace(dir, '')
-      const [_, host, version, ...pathParts] = relativePath.split(path.sep)
+      const [_, host, version] = relativePath.split(path.sep)
 
       // initialize this api host and version if it does not exist yet
-      api[host] = api[host] || { [version]: {} }
+      if (!api.hosts[host]) {
+        api.hosts[host] = { [version]: {} }
+
+        let { dataFile } = config
+        if (!path.isAbsolute(dataFile)) {
+          dataFile = path.join(dir, host, version, dataFile)
+        }
+
+        const dataFileExists = await fs.exists(dataFile)
+        if (dataFileExists) {
+          const urlPath = pathToFileURL(dataFile)
+          const { getData } = await import(urlPath)
+
+          let db = await getData()
+          let schema
+
+          if (db.db) {
+            schema = db.schema
+            db = db.db
+          }
+
+          api.hosts[host][version].db = db
+
+          if (schema) {
+            api.hosts[host][version].schema = schema
+          }
+        }
+      } else if (!api.hosts[host][version]) {
+        api.hosts[host][version] = {}
+      }
 
       // get absolute path for import
       const absPath = path.isAbsolute(file) ? file : path.join(cwd, file)
@@ -52,7 +69,7 @@ export const initApi = async config => {
       const ext = path.extname(file)
       const lambdaPath = pathParts.join('/').replace(ext, '')
 
-      api[host][version][`/${lambdaPath}`] = lambda
+      api.hosts[host][version][`/${lambdaPath}`] = lambda
     }),
   )
 
