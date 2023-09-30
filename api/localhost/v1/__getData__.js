bca394b30ad96a0daaf0c8ed02663e39e9bc69e7
jaeh <jascha@jaeh.at>
2023-09-30 22:54:17 +0200

    api/localhost/v1/__getData__: add some static "db" example data

--- api/localhost/v1/__getData__.js
+++ api/localhost/v1/__getData__.js
@@ -1,14 +1,37 @@
+import * as db from '../../../../../arm/ar/db/index.js'
+import { schema } from '../../../../../arm/ar/db/schema.js'
+
 export const getData = async () => {
-  return await new Promise(r => {
-    setTimeout(() => {
-      r({
-        db: {
-          content: 'this is a string coming from the "db"',
+  return {
+    db: {
+      table: [
+        {
+          slug: 'item1',
+          name: 'item 1',
+          key1: 'item 1 key1',
+          key2: 'item 1 key2',
         },
-        schema: {
-          content: { type: 'string' },
+        {
+          slug: 'item2',
+          name: 'item 2',
+          key1: 'item 2 key1',
+          key2: 'item 2 key2',
         },
-      })
-    }, 10)
-  })
+        {
+          slug: 'item3',
+          name: 'item 3',
+          key1: 'item 3 key1',
+          key2: 'item 3 key2',
+        },
+      ],
+    },
+    schema: {
+      table: {
+        slug: { type: 'slug' },
+        name: { type: 'string', multiple: true },
+        key1: { type: 'string' },
+        key2: { type: 'string' },
+      },
+    },
+  }
 }
