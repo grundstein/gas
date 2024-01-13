47777fabd0d4469512b72e26c224318984ef4138
jaeh <jascha@jaeh.at>
2024-01-13 03:15:04 +0100

    api.js: cleanup, remove commented code, remove console.log

--- src/api.js
+++ src/api.js
@@ -23,7 +23,6 @@ export const initApi = async config => {
 
   // const files = await fs.getFiles(dir)
   const dirs = await fs.getDirectories(dir, { minDepth: 1, maxDepth: 1 })
-  console.log({ dirs })
 
   const dirPromises = dirs.map(async apiDir => {
     const absDir = path.join(cwd, apiDir)
@@ -53,12 +52,9 @@ export const initApi = async config => {
     try {
       const files = await fs.getFiles(absDir)
 
-      console.log({ files })
-
       const lambdaPromises = files
         .filter(file => !file.endsWith(dataFile))
         .map(async file => {
-          console.log(file)
           // get absolute path for import
           const absPath = path.isAbsolute(file) ? file : path.join(cwd, file)
 
@@ -66,11 +62,9 @@ export const initApi = async config => {
 
           // remove the extension from the lambdapath
           const ext = path.extname(file)
-          console.log({ absPath, file })
           const [_, ...pathParts] = file.split(version)
           const lambdaPath = pathParts.join('/').replace(ext, '')
 
-          console.log('registering lambda:', `${version}${lambdaPath}`)
           api[version][lambdaPath] = lambda
         })
 
@@ -81,59 +75,6 @@ export const initApi = async config => {
   })
 
   await Promise.all(dirPromises)
-
-  // if (!path.isAbsolute(dataFile)) {
-  //   dataFile = path.join(dir, version, dataFile)
-  // }
-
-  // const dataFileExists = await fs.exists(dataFile)
-  // console.log('dataFileExists', dataFile, dataFileExists)
-
-  // if (dataFileExists) {
-  //   const urlPath = pathToFileURL(dataFile)
-  //   const { getData } = await import(urlPath)
-
-  //   let db = await getData()
-  //   let schema
-
-  //   if (db.db) {
-  //     schema = db.schema
-  //     db = db.db
-  //   }
-
-  //   const relativePath = dataFile.replace(dir, '')
-  //   const [_, version,] = relativePath.split(path.sep)
-
-  //   api[version].db = db
-
-  //   if (schema) {
-  //     createApiFromSchema({ api, db, schema })
-  //   }
-  // }
-
-  // const lambdaPromises = files
-  //     .filter(file => file.endsWith(dataFile))
-  //     .map(async file => {
-  //       const relativePath = file.replace(dir, '')
-  //       const [_, version, ...pathParts] = relativePath.split(path.sep)
-
-  //       if (!api.hasOwnProperty(version)) {
-  //         api[version] = {}
-  //       }
-
-  //       // get absolute path for import
-  //       const absPath = path.isAbsolute(file) ? file : path.join(cwd, file)
-
-  //       const { default: lambda } = await import(pathToFileURL(absPath))
-
-  //       // remove the extension from the lambdapath
-  //       const ext = path.extname(file)
-  //       const lambdaPath = pathParts.join('/').replace(ext, '')
-
-  //       api[version][`/${lambdaPath}`] = lambda
-  //     })
-
-  // await Promise.all(lambdaPromises)
 
   log.timeTaken(startTime, '@grundstein/gas init took')
 
