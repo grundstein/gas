1555a9883c87325608fb46e337782ce6dee321ae
jaeh <jascha@jaeh.at>
2024-01-13 12:38:51 +0100

    format

--- api/localhost/v1/data.js
+++ api/localhost/v1/data.js
@@ -2,7 +2,6 @@ import http2 from 'node:http2'
 
 const { HTTP2_HEADER_AUTHORITY } = http2.constants
 
-
 export default ({ api, headers }) => {
   const authority = headers[HTTP2_HEADER_AUTHORITY].split(':')[0]
   const { db } = api[authority]
