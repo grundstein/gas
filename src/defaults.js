d29eb361bea3bcc39b0ae96bb562866130e47de0
jaeh <jascha@jaeh.at>
2024-01-15 00:49:36 +0100

    update and cleanup defaults.js

--- src/defaults.js
+++ src/defaults.js
@@ -4,21 +4,11 @@ import { constants } from '@grundstein/commons'
 
 const { HTTP2_HEADER_ORIGIN, HTTP2_HEADER_CONTENT_TYPE, HTTP2_HEADER_ACCEPT } = constants
 
-const certDir = path.join(
-  process.cwd(),
-  'node_modules',
-  '@grundstein',
-  'commons',
-  'src',
-  'certificates',
-)
-
 export const defaults = {
   dir: '/var/www/api',
   host: '0.0.0.0',
   port: 2351,
-  certDir: '/home/grundstein/ca',
+  certDir: '/home/grundstein/certs',
   corsOrigin: '*',
   corsHeaders: `${HTTP2_HEADER_ORIGIN}, x-requested-with, ${HTTP2_HEADER_CONTENT_TYPE}, ${HTTP2_HEADER_ACCEPT}`,
-  certDir,
 }
