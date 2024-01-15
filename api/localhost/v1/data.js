e68494821655a4efc23a012778a608cc8c57f173
jaeh <jascha@jaeh.at>
2024-01-15 01:08:36 +0100

    api/localhost/v1/data: add db, url, hostname and version to params

--- api/localhost/v1/data.js
+++ api/localhost/v1/data.js
@@ -2,9 +2,7 @@ import http2 from 'node:http2'
 
 const { HTTP2_HEADER_AUTHORITY } = http2.constants
 
-export default ({ api, headers }) => {
-  const authority = headers[HTTP2_HEADER_AUTHORITY].split(':')[0]
-  const { db } = api[authority]
+export default ({ api, db, headers, url, hostname, version }) => {
   return {
     code: 200,
     body: JSON.stringify(db.collection),
